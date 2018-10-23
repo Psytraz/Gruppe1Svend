e0c0c84fc5807ee42bad44a805b84f3c60a3ca8d
René Falch Olesen <psytraz@gmail.com>
2018-10-23 11:22:14 +0200

    Virker

---
+++ Gruppe1Svend/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Gruppe1Svend.Models;
+
+namespace Gruppe1Svend.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public IActionResult Ordreoversigt()
+        {
+            return View();
+        }
+
+        public IActionResult Lagerstatus()
+        {
+            ViewData["Message"] = "Lagerstatus";
+
+            return View();
+        }
+
+        public IActionResult Rapport()
+        {
+            ViewData["Message"] = "Rapporter";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
